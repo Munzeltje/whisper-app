ab4f2d2f0a24347cae14a6b4136a7e47d18fd90b
vivian <vivianvanoijen@protonmail.com>
2025-02-04 21:14:29 +0100

    Improve readability.

--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -1,8 +1,9 @@
 from unittest.mock import Mock, patch
 
 import whisper
+from pyannote.audio import Pipeline
 
-from src.transcription import load_whisper_model, transcribe_audio
+from src.transcription import load_whisper_model, transcribe_audio, perform_diarization
 
 
 @patch("whisper.load_model")
@@ -10,10 +11,10 @@ def test_load_whisper_model(mock_load_model):
     test_model_version = "tiny"
     mock_callback = Mock()
 
-    assert (
-        load_whisper_model(test_model_version, mock_callback)
-        == mock_load_model.return_value
-    )
+    result = load_whisper_model(test_model_version, mock_callback)
+    expected = mock_load_model.return_value
+
+    result == expected
     mock_callback.assert_not_called()
 
 
@@ -22,7 +23,9 @@ def test_load_whisper_model_invalid_model_version(mock_load_model):
     test_model_version = "invalid_version"
     mock_callback = Mock()
 
-    assert load_whisper_model(test_model_version, mock_callback) is None
+    result = load_whisper_model(test_model_version, mock_callback)
+
+    assert result is None
     mock_callback.assert_called_once_with(
         "Failed to load Whisper model: Invalid model version: invalid_version"
     )
@@ -35,7 +38,9 @@ def test_load_whisper_model_exception(mock_load_model):
 
     mock_load_model.side_effect = Exception("Something went wrong")
 
-    assert load_whisper_model(test_model_version, mock_callback) is None
+    result = load_whisper_model(test_model_version, mock_callback)
+
+    assert result is None
     mock_callback.assert_called_once_with(
         "Failed to load Whisper model: Something went wrong"
     )
@@ -49,8 +54,7 @@ def test_transcribe_audio():
 
     test_model.transcribe.return_value = "correct"
 
-    assert (
-        transcribe_audio(test_model, test_audio_file, test_language, mock_callback)
-        == "correct"
-    )
+    result = transcribe_audio(test_model, test_audio_file, test_language, mock_callback)
+
+    assert result == "correct"
     mock_callback.assert_not_called()
